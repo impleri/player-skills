7baf6c32c9328a04740c81eb563de2b497d23548
Christopher Roussel <christopher@impleri.net>
2023-03-13 22:18:34 -0500

    style: Remove comments

--- common/src/main/java/net/impleri/playerskills/integration/kubejs/events/EventsBinding.java
+++ common/src/main/java/net/impleri/playerskills/integration/kubejs/events/EventsBinding.java
@@ -2,9 +2,9 @@ package net.impleri.playerskills.integration.kubejs.events;
 
 public abstract class EventsBinding {
 
-    public static final String REGISTRATION = "skills.registration"; //, () -> SkillsRegistrationEventJS.class);
+    public static final String REGISTRATION = "skills.registration";
 
-    public static final String MODIFICATION = "skills.modification"; //, () -> SkillsModificationEventJS.class);
-
-    public static final String SKILL_CHANGED = "skills.onChanged"; //, () -> PlayerSkillChangedEventJS.class).extra(Extra.ID);
+    public static final String MODIFICATION = "skills.modification";
+    
+    public static final String SKILL_CHANGED = "skills.onChanged";
 }
