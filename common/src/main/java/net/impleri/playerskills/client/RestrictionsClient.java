638b760fb611493f1acabd975b2126b7dc777685
Christopher Roussel <christopher@impleri.net>
2023-03-22 21:00:30 -0500

    feature!: Backport dimension and biome facets

--- common/src/main/java/net/impleri/playerskills/client/RestrictionsClient.java
+++ common/src/main/java/net/impleri/playerskills/client/RestrictionsClient.java
@@ -1,9 +1,9 @@
 package net.impleri.playerskills.client;
 
 import net.impleri.playerskills.PlayerSkills;
-import net.impleri.playerskills.api.RestrictionsApi;
 import net.impleri.playerskills.restrictions.AbstractRestriction;
 import net.impleri.playerskills.restrictions.Registry;
+import net.impleri.playerskills.restrictions.RestrictionsApi;
 import net.minecraft.client.Minecraft;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
@@ -11,8 +11,8 @@ import net.minecraft.world.entity.player.Player;
 import java.util.List;
 import java.util.function.Predicate;
 
-public abstract class RestrictionsClient<T, R extends AbstractRestriction<T>> {
-    private static Player getPlayer() {
+public abstract class RestrictionsClient<T, R extends AbstractRestriction<T>, A extends RestrictionsApi<T, R>> {
+    protected static Player getPlayer() {
         try {
             return Minecraft.getInstance().player;
         } catch (Throwable error) {
@@ -24,9 +24,9 @@ public abstract class RestrictionsClient<T, R extends AbstractRestriction<T>> {
 
     private final Registry<R> registry;
 
-    private final RestrictionsApi<T, R> serverApi;
+    protected final A serverApi;
 
-    public RestrictionsClient(Registry<R> registry, RestrictionsApi<T, R> serverApi) {
+    public RestrictionsClient(Registry<R> registry, A serverApi) {
         this.registry = registry;
         this.serverApi = serverApi;
     }
@@ -36,11 +36,7 @@ public abstract class RestrictionsClient<T, R extends AbstractRestriction<T>> {
     }
 
     protected List<R> getFiltered(Predicate<R> predicate) {
-        var player = getPlayer();
-
-        return getAll().stream()
-                .filter(r -> r.condition.test(player) && predicate.test(r))
-                .toList();
+        return serverApi.getFiltered(getPlayer(), predicate);
     }
 
     /**
@@ -48,9 +44,7 @@ public abstract class RestrictionsClient<T, R extends AbstractRestriction<T>> {
      */
     @Deprecated
     public boolean canPlayer(ResourceLocation resource, String fieldName) {
-        var player = getPlayer();
-
-        return serverApi.canPlayer(player, resource, fieldName);
+        return serverApi.canPlayer(getPlayer(), resource, fieldName);
     }
 
     /**
@@ -58,8 +52,6 @@ public abstract class RestrictionsClient<T, R extends AbstractRestriction<T>> {
      */
     @Deprecated
     public boolean canPlayer(T target, String fieldName) {
-        var player = getPlayer();
-
-        return serverApi.canPlayer(player, target, fieldName);
+        return serverApi.canPlayer(getPlayer(), target, fieldName);
     }
 }
--- common/src/main/java/net/impleri/playerskills/restrictions/AbstractRestriction.java
+++ common/src/main/java/net/impleri/playerskills/restrictions/AbstractRestriction.java
@@ -1,39 +1,51 @@
 package net.impleri.playerskills.restrictions;
 
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Predicate;
 
 public abstract class AbstractRestriction<Target> {
     private static final Predicate<Player> DEFAULT_CONDITION = (Player player) -> true;
     public final Target target;
-    public final @NotNull Predicate<Player> condition;
     public final Target replacement;
 
+    public final @NotNull Predicate<Player> condition;
+
+    public final List<ResourceLocation> includeDimensions;
+    public final List<ResourceLocation> excludeDimensions;
+
+    public final List<ResourceLocation> includeBiomes;
+    public final List<ResourceLocation> excludeBiomes;
+
+
+    public AbstractRestriction(
+            Target target,
+            @Nullable Predicate<Player> condition,
+            @Nullable List<ResourceLocation> includeDimensions,
+            @Nullable List<ResourceLocation> excludeDimensions,
+            @Nullable List<ResourceLocation> includeBiomes,
+            @Nullable List<ResourceLocation> excludeBiomes,
+            @Nullable Target replacement
+    ) {
+        this.target = target;
+        this.replacement = replacement;
+        this.condition = (condition != null) ? condition : DEFAULT_CONDITION;
+        this.includeDimensions = (includeDimensions != null) ? includeDimensions : new ArrayList<>();
+        this.excludeDimensions = (excludeDimensions != null) ? excludeDimensions : new ArrayList<>();
+        this.includeBiomes = (includeBiomes != null) ? includeBiomes : new ArrayList<>();
+        this.excludeBiomes = (excludeBiomes != null) ? excludeBiomes : new ArrayList<>();
+    }
 
     public AbstractRestriction(
             Target target,
             @Nullable Predicate<Player> condition,
             @Nullable Target replacement
     ) {
-        this.target = target;
-        this.replacement = replacement;
-        this.condition = (condition != null) ? condition : DEFAULT_CONDITION;
-    }
-
-    public AbstractRestriction(
-            Target target,
-            @NotNull Predicate<Player> condition
-    ) {
-        this(target, condition, null);
-    }
-
-    public AbstractRestriction(
-            Target target,
-            @NotNull Target replacement
-    ) {
-        this(target, null, replacement);
+        this(target, condition, null, null, null, null, replacement);
     }
 }
