723ddcf7fb70a11215418e2e172805ae95d41762
Christopher Roussel <christopher@impleri.net>
2023-04-14 09:18:16 -0500

    feature: Add FTB Teams layer

---
+++ common/src/main/java/net/impleri/playerskills/integration/ftbteams/FTBTeamApi.java
@@ -0,0 +1,44 @@
+package net.impleri.playerskills.integration.ftbteams;
+
+import dev.ftb.mods.ftbteams.FTBTeamsAPI;
+import dev.ftb.mods.ftbteams.data.Team;
+import dev.ftb.mods.ftbteams.data.TeamRank;
+import net.impleri.playerskills.server.TeamApi;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class FTBTeamApi extends TeamApi {
+    public static void registerInstance() {
+        TeamApi.setInstance(new FTBTeamApi());
+    }
+
+    @Nullable
+    private Team getTeam(UUID playerUuid) {
+        return FTBTeamsAPI.getPlayerTeam(playerUuid);
+    }
+
+    @Nullable
+    protected UUID getTeamFor(UUID playerUuid) {
+        var team = getTeam(playerUuid);
+
+        if (team == null) {
+            return null;
+        }
+
+        return team.getId();
+    }
+
+    protected List<UUID> getTeamMembersFor(UUID playerId) {
+        var team = getTeam(playerId);
+
+        if (team == null) {
+            return new ArrayList<>();
+        }
+
+        return team.getRanked(TeamRank.NONE).keySet().stream().toList();
+    }
+
+}
