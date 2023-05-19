c5461bb066cff6ed6008903c5e01ad4de00e1aec
Christopher Roussel <christopher@impleri.net>
2023-05-19 13:56:49 -0500

    refactor: Reorganize Restriction abstractions

---
+++ common/src/main/kotlin/net/impleri/playerskills/integrations/kubejs/api/AbstractRestrictionConditionsBuilder.kt
@@ -0,0 +1,48 @@
+package net.impleri.playerskills.integrations.kubejs.api
+
+import dev.latvian.mods.kubejs.BuilderBase
+import dev.latvian.mods.rhino.util.HideFromJS
+import dev.latvian.mods.rhino.util.RemapForJS
+import net.impleri.playerskills.restrictions.AbstractRestriction
+import net.impleri.playerskills.restrictions.RestrictionConditionsBuilder
+import net.minecraft.resources.ResourceLocation
+import net.minecraft.server.MinecraftServer
+import net.minecraft.world.entity.player.Player
+
+abstract class AbstractRestrictionConditionsBuilder<Target, Restriction : AbstractRestriction<Target>>(
+  id: ResourceLocation?,
+  @HideFromJS override val server: MinecraftServer,
+) : BuilderBase<Restriction>(id),
+  RestrictionConditionsBuilder<Target, PlayerDataJS, Restriction> {
+  @HideFromJS
+  override var rawCondition = { _: PlayerDataJS -> true }
+
+  val condition: (Player) -> Boolean
+    @HideFromJS
+    get() = {
+      rawCondition(PlayerDataJS(it))
+    }
+
+  @HideFromJS
+  override val includeBiomes: MutableList<ResourceLocation> = ArrayList()
+
+  @HideFromJS
+  override val excludeBiomes: MutableList<ResourceLocation> = ArrayList()
+
+  @HideFromJS
+  override val includeDimensions: MutableList<ResourceLocation> = ArrayList()
+
+  @HideFromJS
+  override val excludeDimensions: MutableList<ResourceLocation> = ArrayList()
+
+  @RemapForJS("if")
+  override fun condition(predicate: (PlayerDataJS) -> Boolean): AbstractRestrictionConditionsBuilder<Target, Restriction> {
+    @Suppress("UNCHECKED_CAST")
+    return super.condition(predicate) as AbstractRestrictionConditionsBuilder<Target, Restriction>
+  }
+
+  override fun unless(predicate: (PlayerDataJS) -> Boolean): AbstractRestrictionConditionsBuilder<Target, Restriction> {
+    @Suppress("UNCHECKED_CAST")
+    return super.condition(predicate) as AbstractRestrictionConditionsBuilder<Target, Restriction>
+  }
+}
