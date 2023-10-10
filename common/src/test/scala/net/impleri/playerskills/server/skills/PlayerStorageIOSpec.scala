3f2918143e3f79b4e45e002378f95e8a6cfcfbe0
Christopher Roussel <christopher@impleri.net>
2023-10-09 21:12:24 -0500

    test: Moar tests

--- common/src/test/scala/net/impleri/playerskills/server/skills/PlayerStorageIOSpec.scala
+++ common/src/test/scala/net/impleri/playerskills/server/skills/PlayerStorageIOSpec.scala
@@ -3,7 +3,9 @@ package net.impleri.playerskills.server.skills
 import net.impleri.playerskills.BaseSpec
 import net.impleri.playerskills.api.skills.Skill
 import net.impleri.playerskills.api.skills.SkillTypeOps
+import net.impleri.playerskills.server.skills.storage.FailedToWrite
 import net.impleri.playerskills.server.skills.storage.PersistentStorage
+import net.impleri.playerskills.server.skills.storage.SkillFileMissing
 import net.impleri.playerskills.server.skills.storage.SkillResourceFile
 import net.impleri.playerskills.utils.PlayerSkillsLogger
 import net.minecraft.server.MinecraftServer
@@ -39,6 +41,26 @@ class PlayerStorageIOSpec extends BaseSpec {
     received should be (returnedSkills)
   }
 
+  "PlayerStorageIO.read" should "return an empty list if the file isn't found" in {
+    val storageMock = mock[PersistentStorage]
+    val resourceMock = mock[SkillResourceFile]
+    val typeOpsMock = mock[SkillTypeOps]
+    val loggerMock = mock[PlayerSkillsLogger]
+
+    val givenUuid = UUID.randomUUID()
+
+    val file = new File("/tmp")
+    resourceMock.getPlayerFile(givenUuid) returns file
+
+    storageMock.read(file) returns Left(SkillFileMissing(file))
+
+    val target = new PlayerStorageIO(storageMock, resourceMock, typeOpsMock, loggerMock)
+
+    val received = target.read(givenUuid)
+
+    received should be (List.empty)
+  }
+
   "PlayerStorageIO.write" should "return success" in {
     val storageMock = mock[PersistentStorage]
     val resourceMock = mock[SkillResourceFile]
@@ -61,6 +83,30 @@ class PlayerStorageIOSpec extends BaseSpec {
     val received = target.write(givenUuid, givenSkills)
 
     received should be (true)
+  }
+
+  "PlayerStorageIO.write" should "return failure if the write fails" in {
+    val storageMock = mock[PersistentStorage]
+    val resourceMock = mock[SkillResourceFile]
+    val typeOpsMock = mock[SkillTypeOps]
+    val loggerMock = mock[PlayerSkillsLogger]
+
+    val givenUuid = UUID.randomUUID()
+    val givenSkills = List(TestSkill())
+
+    val serializedSkill = "test-skill"
+    typeOpsMock.serialize(TestSkill()) returns Some(serializedSkill)
+
+    val file = new File("/tmp")
+    resourceMock.getPlayerFile(givenUuid) returns file
+
+    storageMock.write(file, List(serializedSkill)) returns Left(FailedToWrite(file))
+
+    val target = new PlayerStorageIO(storageMock, resourceMock, typeOpsMock, loggerMock)
+
+    val received = target.write(givenUuid, givenSkills)
+
+    received should be (false)
   }
 
   "PlayerStorageIO.apply" should "return the correct class" in {
