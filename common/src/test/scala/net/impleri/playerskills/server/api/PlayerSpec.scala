2ca9315a09c2cd1629e4f0fc7a645b777c3ed91b
Christopher Roussel <christopher@impleri.net>
2023-10-26 21:47:00 -0500

    test: Updates for Player API tests

---
+++ common/src/test/scala/net/impleri/playerskills/server/api/PlayerSpec.scala
@@ -0,0 +1,339 @@
+package net.impleri.playerskills.server.api
+
+import net.impleri.playerskills.BaseSpec
+import net.impleri.playerskills.api.skills.ChangeableSkillOps
+import net.impleri.playerskills.api.skills.Skill
+import net.impleri.playerskills.api.skills.SkillOps
+import net.impleri.playerskills.api.skills.SkillType
+import net.impleri.playerskills.api.skills.SkillTypeOps
+import net.impleri.playerskills.server.skills.PlayerRegistry
+import net.minecraft.resources.ResourceLocation
+
+import java.util.UUID
+
+class PlayerSpec extends BaseSpec {
+  private val registryMock = mock[PlayerRegistry]
+  private val skillTypeOpsMock = mock[SkillTypeOps]
+  private val skillOpsMock = mock[SkillOps]
+
+  private val testUnit = new Player(registryMock, skillTypeOpsMock, skillOpsMock)
+
+  "PlayerRegistryFacade.get" should "proxy PlayerRegistry.get" in {
+    val givenUuid = UUID.randomUUID()
+    val expected = List.empty[Skill[_]]
+
+    registryMock.get(givenUuid) returns expected
+
+    testUnit.get(givenUuid) should be(expected)
+  }
+
+  it should "return a skill matching the given name" in {
+    val givenUuid = UUID.randomUUID()
+    val givenName = new ResourceLocation("skillstest", "test_skill")
+    val expected = mock[Skill[_]]
+    expected.name returns givenName
+
+    val returnedList = List(expected)
+
+    registryMock.get(givenUuid) returns returnedList
+
+    testUnit.get(givenUuid, givenName).value should be(expected)
+  }
+
+  it should "return None if it cannot find a skill matching the given name" in {
+    val givenUuid = UUID.randomUUID()
+    val givenName = new ResourceLocation("skillstest", "test_skill")
+    val expected = mock[Skill[_]]
+
+    val returnedList = List(expected)
+
+    registryMock.get(givenUuid) returns returnedList
+
+    testUnit.get(givenUuid, givenName) should be(None)
+  }
+
+  "PlayerRegistryFacade.isOnline" should "proxy PlayerRegistry.has" in {
+    val givenUuid = UUID.randomUUID()
+    val expected = false
+
+    registryMock.has(givenUuid) returns expected
+
+    testUnit.isOnline(givenUuid) should be(expected)
+  }
+
+  "PlayerRegistryFacade.open" should "proxy PlayerRegistry.open" in {
+    val givenUuid = UUID.randomUUID()
+    val expected = List.empty[Skill[_]]
+
+    registryMock.open(givenUuid) returns expected
+
+    testUnit.open(givenUuid) should be(expected)
+  }
+
+  it should "proxy PlayerRegistry.open with a list" in {
+    val givenUuid = UUID.randomUUID()
+    val givenList = List(givenUuid, UUID.randomUUID())
+    val expected = List(givenUuid)
+
+    registryMock.open(givenList) returns expected
+
+    testUnit.open(givenList) should be(expected)
+  }
+
+  "PlayerRegistryFacade.upsert" should "proxy PlayerRegistry.upsert" in {
+    val givenUuid = UUID.randomUUID()
+    val givenSkill = mock[Skill[_]]
+    val expected = List(givenSkill)
+
+    registryMock.upsert(givenUuid, givenSkill) returns expected
+
+    testUnit.upsert(givenUuid, givenSkill) should be(expected)
+  }
+
+  "PlayerRegistryFacade.close" should "proxy PlayerRegistry.close" in {
+    val givenUuid = UUID.randomUUID()
+    val expected = false
+
+    registryMock.close(givenUuid) returns expected
+
+    testUnit.close(givenUuid) should be(expected)
+  }
+
+  it should "proxy PlayerRegistry.close with a list" in {
+    val givenUuid = UUID.randomUUID()
+    val givenList = List(givenUuid, UUID.randomUUID())
+    val returnedList = List(givenUuid)
+
+    registryMock.close(givenList) returns returnedList
+
+    testUnit.close(givenList) should be(true)
+  }
+
+  "Player.can" should "call the appropriate SkillType.can with the player's skill if both are found" in {
+    val givenUuid = UUID.randomUUID()
+    val skillName = new ResourceLocation("skillstest", "test_skill")
+    val givenThreshold = Option("test-value")
+
+    val givenSkill = mock[Skill[String]]
+    givenSkill.name returns skillName
+
+    val givenSkillType = mock[SkillType[String]]
+    skillTypeOpsMock.get(givenSkill) returns Option(givenSkillType)
+
+    val otherSkill = mock[Skill[String]]
+    otherSkill.name returns new ResourceLocation("skillstest", "other_skill")
+    val foundSkill = mock[Skill[String]]
+    foundSkill.name returns skillName
+    val foundSkills = List(otherSkill, foundSkill)
+    registryMock.get(givenUuid) returns foundSkills
+
+    val expected = false
+    givenSkillType.can(foundSkill, givenThreshold) returns expected
+
+    testUnit.can(givenUuid, givenSkill, givenThreshold) should be(expected)
+  }
+
+  it should "return default value if the SkillType is not found" in {
+    val givenUuid = UUID.randomUUID()
+    val skillName = new ResourceLocation("skillstest", "test_skill")
+
+    val givenSkill = mock[Skill[String]]
+    givenSkill.name returns skillName
+
+    skillTypeOpsMock.get(givenSkill) returns None
+
+    val otherSkill = mock[Skill[String]]
+    otherSkill.name returns new ResourceLocation("skillstest", "other_skill")
+    val foundSkill = mock[Skill[String]]
+    foundSkill.name returns skillName
+    val foundSkills = List(otherSkill, foundSkill)
+    registryMock.get(givenUuid) returns foundSkills
+
+    testUnit.can(givenUuid, givenSkill) should be(Player.DEFAULT_SKILL_RESPONSE)
+  }
+
+  it should "return default value if the skill is not found for the player" in {
+    val givenUuid = UUID.randomUUID()
+    val skillName = new ResourceLocation("skillstest", "test_skill")
+
+    val givenSkill = mock[Skill[String]]
+    givenSkill.name returns skillName
+
+    val givenSkillType = mock[SkillType[String]]
+    skillTypeOpsMock.get(givenSkill) returns Option(givenSkillType)
+
+    val otherSkill = mock[Skill[String]]
+    otherSkill.name returns new ResourceLocation("skillstest", "other_skill")
+
+    val foundSkills = List(otherSkill)
+    registryMock.get(givenUuid) returns foundSkills
+
+    testUnit.can(givenUuid, givenSkill) should be(Player.DEFAULT_SKILL_RESPONSE)
+
+    givenSkillType.can(*, None) wasNever called
+  }
+
+  "Player.reset" should "upsert player with the default skill value" in {
+    val givenUuid = UUID.randomUUID()
+    val skillName = new ResourceLocation("skillstest", "test_skill")
+
+    val givenSkill = mock[Skill[String]]
+    givenSkill.name returns skillName
+
+    val foundSkill = mock[Skill[String]]
+    skillOpsMock.get[String](skillName) returns Option(foundSkill)
+
+    val expected = List(foundSkill)
+    registryMock.upsert(givenUuid, foundSkill) returns expected
+
+    testUnit.reset(givenUuid, givenSkill) should be(expected)
+  }
+
+  it should "do nothing if the default skill value cannot be found" in {
+    val givenUuid = UUID.randomUUID()
+    val skillName = new ResourceLocation("skillstest", "test_skill")
+
+    val givenSkill = mock[Skill[String]]
+    givenSkill.name returns skillName
+
+    skillOpsMock.get[String](skillName) returns None
+
+    testUnit.reset(givenUuid, givenSkill) should be(List.empty)
+
+    registryMock.upsert(givenUuid, *) wasNever called
+  }
+
+  "Player.calculateValue" should "return a new valid value" in {
+    val givenUuid = UUID.randomUUID()
+    val skillName = new ResourceLocation("skillstest", "test_skill")
+
+    val givenSkill = mock[Skill[String]]
+    givenSkill.name returns skillName
+
+    val givenValue = Option("test-value")
+
+    val foundSkill = mock[Skill[String] with ChangeableSkillOps[String, Skill[String]]]
+    foundSkill.name returns skillName
+    foundSkill.areChangesAllowed() returns true
+    foundSkill.isAllowedValue(givenValue) returns true
+    foundSkill.value returns None
+
+    val foundSkills = List(foundSkill)
+    registryMock.get(givenUuid) returns foundSkills
+
+    val createdSkill = mock[Skill[String]]
+    foundSkill.mutate(givenValue) returns createdSkill
+
+    testUnit.calculateValue(givenUuid, givenSkill, givenValue).value should be(createdSkill)
+
+    skillOpsMock.get(skillName) wasNever called
+  }
+
+  it should "return a new valid value using the default skill" in {
+    val givenUuid = UUID.randomUUID()
+    val skillName = new ResourceLocation("skillstest", "test_skill")
+
+    val givenSkill = mock[Skill[String]]
+    givenSkill.name returns skillName
+
+    val givenValue = Option("test-value")
+
+    val foundSkill = mock[Skill[String] with ChangeableSkillOps[String, Skill[String]]]
+    foundSkill.name returns skillName
+    foundSkill.areChangesAllowed() returns true
+    foundSkill.isAllowedValue(givenValue) returns true
+    foundSkill.value returns None
+
+    registryMock.get(givenUuid) returns List.empty
+    skillOpsMock.get[String](skillName) returns Option(foundSkill)
+
+    val createdSkill = mock[Skill[String]]
+    foundSkill.mutate(givenValue) returns createdSkill
+
+    testUnit.calculateValue(givenUuid, givenSkill, givenValue).value should be(createdSkill)
+  }
+
+  it should "returns None if the skill cannot be changed" in {
+    val givenUuid = UUID.randomUUID()
+    val skillName = new ResourceLocation("skillstest", "test_skill")
+
+    val givenSkill = mock[Skill[String]]
+    givenSkill.name returns skillName
+
+    val givenValue = Option("test-value")
+
+    val foundSkill = mock[Skill[String] with ChangeableSkillOps[String, Skill[String]]]
+    foundSkill.name returns skillName
+    foundSkill.areChangesAllowed() returns false
+    foundSkill.isAllowedValue(givenValue) returns true
+    foundSkill.value returns None
+
+    registryMock.get(givenUuid) returns List(foundSkill)
+
+    val createdSkill = mock[Skill[String]]
+    foundSkill.mutate(givenValue) returns createdSkill
+
+    testUnit.calculateValue(givenUuid, givenSkill, givenValue) should be(None)
+
+    skillOpsMock.get(skillName) wasNever called
+    foundSkill.mutate(*) wasNever called
+  }
+
+  it should "returns None if the value is not valid" in {
+    val givenUuid = UUID.randomUUID()
+    val skillName = new ResourceLocation("skillstest", "test_skill")
+
+    val givenSkill = mock[Skill[String]]
+    givenSkill.name returns skillName
+
+    val givenValue = Option("test-value")
+
+    val foundSkill = mock[Skill[String] with ChangeableSkillOps[String, Skill[String]]]
+    foundSkill.name returns skillName
+    foundSkill.areChangesAllowed() returns true
+    foundSkill.isAllowedValue(givenValue) returns false
+    foundSkill.value returns None
+
+    registryMock.get(givenUuid) returns List(foundSkill)
+
+    val createdSkill = mock[Skill[String]]
+    foundSkill.mutate(givenValue) returns createdSkill
+
+    testUnit.calculateValue(givenUuid, givenSkill, givenValue) should be(None)
+
+    skillOpsMock.get(skillName) wasNever called
+    foundSkill.mutate(*) wasNever called
+  }
+
+  it should "returns None if the value has not changed" in {
+    val givenUuid = UUID.randomUUID()
+    val skillName = new ResourceLocation("skillstest", "test_skill")
+
+    val givenSkill = mock[Skill[String]]
+    givenSkill.name returns skillName
+
+    val givenValue = Option("test-value")
+
+    val foundSkill = mock[Skill[String] with ChangeableSkillOps[String, Skill[String]]]
+    foundSkill.name returns skillName
+    foundSkill.areChangesAllowed() returns true
+    foundSkill.isAllowedValue(givenValue) returns true
+    foundSkill.value returns givenValue
+
+    registryMock.get(givenUuid) returns List(foundSkill)
+
+    val createdSkill = mock[Skill[String]]
+    foundSkill.mutate(givenValue) returns createdSkill
+
+    testUnit.calculateValue(givenUuid, givenSkill, givenValue) should be(None)
+
+    skillOpsMock.get(skillName) wasNever called
+    foundSkill.mutate(*) wasNever called
+  }
+
+  "Player.apply" should "return a Player instance" in {
+    val facade = Player(registryMock, skillTypeOpsMock, skillOpsMock)
+
+    facade.isInstanceOf[Player] should be(true)
+  }
+}
