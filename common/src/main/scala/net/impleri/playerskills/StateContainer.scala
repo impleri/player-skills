ef60437ea3faaa191b0ccc9d18910cfdfec40f75
Christopher Roussel <christopher@impleri.net>
2023-10-31 21:57:06 -0500

    refactor: Add stateful contructor methods to create classes that depend on stateful values

--- common/src/main/scala/net/impleri/playerskills/StateContainer.scala
+++ common/src/main/scala/net/impleri/playerskills/StateContainer.scala
@@ -1,5 +1,9 @@
 package net.impleri.playerskills
 
+import net.impleri.playerskills.api.skills.Skill
+import net.impleri.playerskills.api.skills.SkillOps
+import net.impleri.playerskills.api.skills.SkillType
+import net.impleri.playerskills.api.skills.SkillTypeOps
 import net.impleri.playerskills.events.handlers.EventHandlers
 import net.impleri.playerskills.skills.SkillRegistry
 import net.impleri.playerskills.skills.SkillTypeRegistry
@@ -19,4 +23,8 @@ case class StateContainer() {
     SKILL_TYPES.resync()
     SKILLS.resync()
   }
+
+  def getSkillTypeOps: SkillTypeOps = SkillType(SKILL_TYPES)
+
+  def getSkillOps: SkillOps = Skill(getSkillTypeOps, SKILLS)
 }
