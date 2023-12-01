189c0247b219af192083372ed3606c7665199516
Christopher Roussel <christopher@impleri.net>
2023-11-30 22:00:06 -0600

    refactor: Make the Network facade more easily mockable

--- common/src/main/scala/net/impleri/playerskills/facades/architectury/Network.scala
+++ common/src/main/scala/net/impleri/playerskills/facades/architectury/Network.scala
@@ -7,9 +7,7 @@ import dev.architectury.networking.simple.SimpleNetworkManager
 import net.impleri.playerskills.PlayerSkills
 import net.minecraft.network.FriendlyByteBuf
 
-case class Network(namespace: String = PlayerSkills.MOD_ID) {
-  private val networkManager = SimpleNetworkManager.create(namespace)
-
+case class Network(private val networkManager: SimpleNetworkManager) {
   def registerClientboundMessage[T <: BaseS2CMessage](name: String, factory: FriendlyByteBuf => T): MessageType = {
     networkManager.registerS2C(name, b => factory(b))
   }
@@ -17,4 +15,8 @@ case class Network(namespace: String = PlayerSkills.MOD_ID) {
   def registerServerboundMessage[T <: BaseC2SMessage](name: String, factory: FriendlyByteBuf => T): MessageType = {
     networkManager.registerC2S(name, b => factory(b))
   }
+}
+
+object Network {
+  def apply(namespace: String = PlayerSkills.MOD_ID): Network = Network(SimpleNetworkManager.create(namespace))
 }
