c7cb373176ee9c2322fd98cf485ad5bf5d3c40e8
Christopher Roussel <christopher@impleri.net>
2024-02-25 22:31:25 -0600

    refactor: Abstract out IsRecipe trait for future reuse

--- common/src/main/scala/net/impleri/playerskills/facades/minecraft/crafting/Recipe.scala
+++ common/src/main/scala/net/impleri/playerskills/facades/minecraft/crafting/Recipe.scala
@@ -7,20 +7,33 @@ import net.minecraft.world.item.crafting.{Recipe => McRecipe}
 import net.minecraft.world.Container
 import net.minecraft.world.item.crafting.Ingredient
 import net.minecraft.world.item.ItemStack
+import net.minecraft.world.item.crafting.RecipeType
 
 import scala.jdk.CollectionConverters._
 import scala.util.chaining.scalaUtilChainingOps
 
-case class Recipe[C <: Container](private val data: McRecipe[C]) extends HasName {
+trait IsRecipe {
+  def getResult: ItemStack
+
+  def getResultItem: Item
+
+  def getIngredients: List[Ingredient]
+
+  def getIngredientItems: List[Item] = getIngredients.flatMap(_.getItems).map(Item(_))
+
+  def getOutput: Item = getResultItem
+}
+
+case class Recipe[C <: Container](private val data: McRecipe[C]) extends IsRecipe with HasName {
+  def getRaw: McRecipe[C] = data
+
+  def getType: RecipeType[McRecipe[C]] = data.getType.asInstanceOf[RecipeType[McRecipe[C]]]
+
   def getResult: ItemStack = data.getResultItem
 
   def getResultItem: Item = getResult.pipe(Item(_))
 
   def getIngredients: List[Ingredient] = data.getIngredients.asScala.toList
-
-  def getIngredientItems: List[Item] = getIngredients.flatMap(_.getItems).map(Item(_))
-
-  def getOutput: Item = getResultItem
 
   override def getName: Option[ResourceLocation] = Option(data.getId).map(ResourceLocation(_))
 }
